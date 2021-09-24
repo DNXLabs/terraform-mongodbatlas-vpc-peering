3d0919a879d300d9e2b4429d968a80902583e2ac
Maicon Alegre <maicon.rocha@dnx.solutions>
2021-09-24 03:57:58 +0000

    remove routes from public subnet

--- routes.tf
+++ routes.tf
@@ -1,13 +1,13 @@
-resource "aws_route" "accepter_public" {
-  provider = aws
-  count    = length(distinct(data.aws_route_table.accepter_public.*.route_table_id))
-  route_table_id = element(
-    distinct(data.aws_route_table.accepter_public.*.route_table_id),
-    count.index
-  )
-  destination_cidr_block    = var.atlas_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.accepter.id
-}
+# resource "aws_route" "accepter_public" {
+#   provider = aws
+#   count    = length(distinct(data.aws_route_table.accepter_public.*.route_table_id))
+#   route_table_id = element(
+#     distinct(data.aws_route_table.accepter_public.*.route_table_id),
+#     count.index
+#   )
+#   destination_cidr_block    = var.atlas_cidr_block
+#   vpc_peering_connection_id = aws_vpc_peering_connection_accepter.accepter.id
+# }
 
 resource "aws_route" "accepter_private" {
   provider = aws
