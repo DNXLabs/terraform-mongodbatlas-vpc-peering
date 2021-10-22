c6e293e0bfc3157315cb37c155eda15e532beb87
Maicon Alegre <maicon.rocha@dnx.solutions>
2021-10-22 03:49:37 +0000

    add mongodbatlas_network_container to be created as part of the module

--- vpc-peering.tf
+++ vpc-peering.tf
@@ -5,11 +5,19 @@
 #   region_name      = var.atlas_region
 # }
 
+
+resource "mongodbatlas_network_container" "network_container" {
+  project_id       = var.atlas_project_id
+  atlas_cidr_block = var.atlas_cidr_block
+  provider_name    = "AWS"
+  region_name      = var.atlas_region
+}
+
 # Create the peering connection request
 resource "mongodbatlas_network_peering" "aws_settings" {
   accepter_region_name   = var.aws_region_name
   project_id             = var.atlas_project_id
-  container_id           = var.atlas_container_id #mongodbatlas_network_container.atlas_settings.id
+  container_id           = mongodbatlas_network_container.network_container.id
   provider_name          = "AWS"
   route_table_cidr_block = var.aws_cidr_block
   vpc_id                 = var.aws_vpc_id
